35567d67cc9cdd70fe0dc17ca16ea27778c063c9
Konstantin <levachin@luckandroll.com>
2020-07-09 07:27:59 +0300

    added save rooms

--- server.js
+++ server.js
@@ -3,12 +3,29 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+app.use(express.json());
+
 const rooms = new Map();
 
 app.get('/rooms', (req, res) => {
     console.log('rooms');
     rooms.set('test', '123')
     res.json(rooms);
+});
+
+app.post('/rooms', (req, res) => {
+    const { room, userName } = req.body;
+    if (!rooms.has(room)) {
+        rooms.set(
+            room,
+            new Map([
+                ['users', new Map()],
+                ['messages', []],
+            ]),
+        );
+    }
+    console.log(rooms);
+    res.send();
 });
 
 io.on('connection', (socket) => {
--- src/components/BlockJoin/BlockJoin.jsx
+++ src/components/BlockJoin/BlockJoin.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import axios from 'axios'
 import socket from '../../socket';
 
 function BlockJoin() {
+    const [room, setRoom] = React.useState('')
+    const [userName, setUserName] = React.useState('')
+
+    const onEnter = () => {
+        if (!room || !userName) {
+            return alert('Incorrect data')
+        }
+        console.log(room, userName)
+        axios.post('/rooms', {room, userName})
+    }
 
     return (
         <div className='block-join'>
-            <input type="text" placeholder='room'/>
-            <input type="text" placeholder='name'/>
-            <button className='btn btn-info'>Войти</button>
+            <input type="text" placeholder='room' value={room} onChange={(e) => setRoom(e.target.value) }/>
+            <input type="text" placeholder='name' value={userName} onChange={(e) => setUserName(e.target.value) }/>
+            <button onClick={onEnter} className='btn btn-info'>Войти</button>
         </div>
     );
 }
